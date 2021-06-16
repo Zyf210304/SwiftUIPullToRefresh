bd4511fd2383f26505547fc27986e505ad0a3c64
张亚飞 <672233580@qq.com>
2021-06-16 10:25:38 +0800

    Initial Commit

---
+++ SwiftUIPullToRefresh/SwiftUIPullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPullToRefreshApp.swift
+//  SwiftUIPullToRefresh
+//
+//  Created by 张亚飞 on 2021/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
